8c0a2788cc8ef2901b1232410044908a017db2b1
Giovanni Orellana <gioorellana1@gmail.com>
2017-06-08 16:29:22 -0600

    Refactoizando la estructura del proyecto
    Agregando carpeta de Components con los headers, main y archivos css funcionando.

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import './header.css'
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="root">
+      <h1 className="logo">Cenro Operaciones 74-02</h1>
+      </header>
+
+    )
+  }
+}
+
+export default Header
